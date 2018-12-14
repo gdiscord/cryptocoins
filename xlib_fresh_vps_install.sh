bf8284ee1e5fc4572aa31de02a074d1d4204476c
gdiscord <43574012+gdiscord@users.noreply.github.com>
2018-12-14 00:48:59 +0000

    Create xlib_fresh_vps_install.sh

---
+++ xlib_fresh_vps_install.sh
@@ -0,0 +1,114 @@
+#!/bin/sh
+
+CONF_FILE="/home/xlibmn/.liberty/liberty.conf"
+DEFAULT_RPC_PORT=10416
+PORT=10417
+MASTERNODE_GEN_KEY=$1
+
+if [ -z "$MASTERNODE_GEN_KEY" ] 
+then
+	echo "|||!!!!!!                                 !!!!!!!|||"
+	echo "|||!!!!!!   STOP on MISSING INFORMATION   !!!!!!!|||"
+	echo "                                                    "
+	echo "        ---------------------------------           "
+	echo "                                                    " 
+	echo "       Masternode private key is missing.           "   
+	echo "                                                    "   
+	echo "  1. Open debug console on your desktop wallet.     " 
+	echo "                                                    " 
+	echo "          2. type: createmasternodekey.             "   
+	echo "                                                    " 
+	echo "   Then use that to start the script as below:      "    
+	echo "                                                    "
+	echo "           $0 <masternode_gen_key>                  "    
+	echo "                                                    "
+	echo "======  ------------------------------------- ======"   
+	echo "===================================================="
+	echo "===================================================="
+	exit 
+fi
+
+
+apt install make
+apt install aptitude -y
+apt-get update -y
+apt-get upgrade -y
+
+apt-get install fail2ban -y
+cp /etc/fail2ban/fail2ban.conf /etc/fail2ban/fail2ban.local
+cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
+
+apt install tzdata
+
+fallocate -l 3000M /mnt/3000MB.swap
+dd if=/dev/zero of=/mnt/3000MB.swap bs=1024 count=3072000
+mkswap /mnt/3000MB.swap
+swapon /mnt/3000MB.swap
+chmod 600 /mnt/3000MB.swap
+echo '/mnt/3000MB.swap  none  swap  sw 0  0' >> /etc/fstab
+
+useradd -m -s /bin/bash xlibmn
+
+wget https://s3.amazonaws.com/liberty-builds/5.0.58.0/linux-x64.tar.gz
+sudo tar xvzf linux-x64.tar.gz -C /usr/local/bin/
+rm linux-x64.tar.gz
+
+su xlibmn -c "libertyd -daemon"
+
+function create_conf {
+	if ! apt-get -qq install pwgen; 
+	    then
+			sudo apt-get install pwgen
+	fi
+	if ! apt-get -qq install dnsutils; 
+		then 
+			sudo apt-get install dnsutils
+	fi
+	RPC_USER=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+	PASSWORD=$(pwgen -s 64 1)
+
+echo "creating conf file"
+su xlibmn -c "liberty-cli stop" && sleep 5
+
+cat <<EOF > "$CONF_FILE"
+rpcuser=$RPC_USER
+rpcpassword=$PASSWORD
+rpcallowip=127.0.0.1
+rpcport=$DEFAULT_RPC_PORT
+port=$PORT
+listen=1
+server=1
+daemon=1
+maxconnections=256
+masternode=1
+masternodeadd=$WANIP
+masternodeprivkey=$MASTERNODE_GEN_KEY
+EOF
+cat "$CONF_FILE"
+
+}
+create_conf
+su xlibmn -c libertyd
+echo "please wait... just seconds to go!"
+sleep 15
+su xlibmn -c "liberty-cli getblockcount"
+
+
+echo "|||!!!!!!!!!!!!!!!!!!!!!                            !!!!!!!!!!!!!!!!!!!!"
+echo "|||!!!!!!!!!!!!!!!!!!!!!   !!!CONGRATULATIONS!!!    !!!!!!!!!!!!!!!!!!!!"
+echo "                                                                       "
+echo "              --------------------------------                         "
+echo "                                                                       " 
+echo "       Your Liberty wallet is deployed and running on VPS.             "
+echo "                                                                       " 
+echo "         The public ip address of your Masternode is $WANIP.           "  
+echo "                                                                       "   
+echo "               Use this ip to setup your desktop wallet.               "    
+echo "                                                                       "   
+echo "                                                                       " 
+echo "    Remember to use the same masternodeprivkey in your desktop wallet  "        
+echo "                                                                       "
+echo "                                                                       " 
+echo "=========   --------------------------------------------   ============"   
+echo "======================================================================="
+echo "======================================================================="
